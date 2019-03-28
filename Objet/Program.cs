8e6c318bcf8acbff0fca81f9cf24cd109f08564f
SOVILLA Flavio <Flavio.SOVILLA@cpnv.ch>
2019-03-28 11:41:09 +0100

    Projet Complet debut

---
+++ Objet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+
+            Console.Write("Press Enter to leave : ");
+            Console.Read();
+
+        }
+    }
+}
